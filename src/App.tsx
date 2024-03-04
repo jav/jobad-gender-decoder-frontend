347420d7db50cd76a91878e494200adf0cae0a25
Javier Ubillos <javier@ubillos.org>
2024-03-04 22:46:39 +0100

    Add scaffold for interactive textfield and b utton.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const backendUrl = "http://localhost:3300"
+
+  const [jobAdInputText, setJobAdInputText] = useState("Insert job ad here...")
+  const [feedbackSubmissionSuccess, setFeedbackSubmissionSuccess] = useState("No info")
+  const [jobAdFeedback, setJobAdFeedback] = useState("No feedback yet...")
+
+  const getFeedbackOnJobAd = async () => {
+    try {
+      const res = await fetch(backendUrl + "/getFeedbackOnJobAd", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          jobAd: jobAdInputText
+        })
+      })
+      const data = await res.json()
+      console.log(data)
+      setFeedbackSubmissionSuccess("Success")
+      setJobAdFeedback(data.data.feedback)
+    } catch (error) {
+      console.log(error)
+      setFeedbackSubmissionSuccess("Error")
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className="JobAdInput" cols={100} rows={20} value={jobAdInputText} onChange={e => setJobAdInputText(e.target.value)}></textarea>
+      <button className="GetFeedbackOnJobAd" onClick={getFeedbackOnJobAd}>Get feedback on ad</button><span>{feedbackSubmissionSuccess}</span>
+      <textarea className="JobAdOutput" readOnly={true} cols={100} rows={20} value={jobAdFeedback}></textarea>
     </div>
   );
 }

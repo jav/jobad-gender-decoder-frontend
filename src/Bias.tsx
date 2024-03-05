012cc1a8392ba5b61a0a1d439c62a0636642c77f
Javier Ubillos <javier@ubillos.org>
2024-03-05 22:36:01 +0100

    Change layout of <Bias> to horizontal.

--- src/Bias.tsx
+++ src/Bias.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Paper, Typography } from '@mui/material';
 
 interface BiasProps {
     masculineWordCount: number
@@ -9,15 +9,19 @@ const Bias = (props: BiasProps) => {
     const { masculineWordCount, femenineWordCount } = props
 
     return (
-        <Container>
-            <Typography variant="h6">Femenine word count: {femenineWordCount}</Typography>
-            <Typography variant="h6">Masculine word count: {masculineWordCount}</Typography>
-            <Typography variant="body1">Recommendation: Keep the masculine word count low.</Typography>
-            <Typography variant="body1">
-                A high masculine bias will discourage female applicants. <br />
-                A high femenine count has a negligable impact on male applicants.
-            </Typography>
-        </Container>
+        <Paper sx={{ display: "flex", flexDirection: "row", my: 4, padding:4 }}>
+            <Container>
+                <Typography variant="h6">Femenine word count: {femenineWordCount}</Typography>
+                <Typography variant="h6">Masculine word count: {masculineWordCount}</Typography>
+            </Container>
+            <Container>
+                <Typography variant="body1">Recommendation: Keep the masculine word count low.</Typography>
+                <Typography variant="body1">
+                    A high masculine bias will discourage female applicants. <br />
+                    A high femenine count has a negligable impact on male applicants.
+                </Typography>
+            </Container>
+        </Paper>
     )
 }
 
